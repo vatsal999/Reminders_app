d6d2b2bc25f05eea4a9f03180fd83d956d299b75
vatsal999 <68118780+vatsal999@users.noreply.github.com>
2023-04-29 10:39:30 +0530

    onDelete function with database

--- ReminderDVM/ViewModels/ReminderListModel.swift
+++ ReminderDVM/ViewModels/ReminderListModel.swift
@@ -18,23 +18,19 @@ class ReminderListModel : ObservableObject{
     
     func getItems(){
         self.reminders = self.db.read()
-        /*let newItems = [
-            Reminder(title: "Buy Tickets", body: "buy them early!", date: Date(timeIntervalSinceNow: 15), status: true),
-            Reminder(title: "Donate organs", body: "Nirmaan calling", date: Date(timeIntervalSinceNow: 15), status: true),
-            Reminder(title: "Prepare for midsems", body: "10cg this sem", date: Date(timeIntervalSinceNow: 15), status: false),
-            Reminder(title: "Sleep", body: "pepe feels bad man", date: Date(timeIntervalSinceNow: 15), status: false),
-        ]
-        reminders.append(contentsOf: newItems)*/
     }
     
     func addItem(title: String, body: String, date: Date){
         //let newItem = Reminder(title: title, body: body, date: date, status: false)
         //reminders.append(newItem)
-        db.insert(title: title, body: body, date: date, status: false)
+        self.db.insert(title: title, body: body, date: date, status: false)
         self.reminders = self.db.read()
     }
     
     func deleteItem(indexSet: IndexSet){
+        for index in indexSet {
+            self.db.deleteByID(id: index)
+        }
         reminders.remove(atOffsets: indexSet)
     }
     
